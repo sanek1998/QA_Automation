555f030174d6ce84ed24f0a3d58579923f67457f
AlexanderErmolin <alexanderermolin@coherentsolutions.com>
2021-03-05 14:08:48 +0300

    Task90 is ready for check

---
+++ Task90/Task90/Tests/TestBase.cs
@@ -0,0 +1,27 @@
+﻿using Allure.Commons;
+using NUnit.Framework;
+using Task90.App;
+
+namespace Task90.Tests
+{
+    public class TestBase: AllureReport
+    {
+        public Application App;
+
+        [SetUp]
+        public void Start()
+        {
+            App = new Application();
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            App.TestLoggingAsScreenshot();
+            App.Quit();
+            App = null;
+
+        }
+
+    }
+}
---
+++ Task90/Task90/Tests/UserAuthorizationTest.cs
@@ -0,0 +1,38 @@
+﻿using Allure.Commons.Model;
+using Allure.NUnit.Attributes;
+using NUnit.Framework;
+using Task90.Model;
+
+namespace Task90.Tests
+{
+    [TestFixture]
+    public class UserAuthorizationTest : TestBase
+    {
+        [AllureSubSuite("Login") ]
+        [AllureSeverity(SeverityLevel.Critical)]
+        [AllureLink("https://github.com/sanek1998/QA_Automation")]
+        [AllureTest("This test opens website tut.by and tries to login")]
+        [AllureOwner("Ermolin Alexander")]
+        [Test, TestCaseSource(typeof(DataProviders), "ValidUsers")]
+        public void CanUserLogin(User user)
+        {
+            App.UserLogin(user);
+            Assert.True(App.LoggedIn(), "Login to account failed");
+        }
+
+        [AllureSubSuite("Logout")]
+        [AllureSeverity(SeverityLevel.Critical)]
+        [AllureLink("https://github.com/sanek1998/QA_Automation")]
+        [AllureTest("This test opens website tut.by and tries to login then tries to logout")]
+        [AllureOwner("Ermolin Alexander")]
+
+
+        [Test, TestCaseSource(typeof(DataProviders), "ValidUsers")]
+        public void CanUserLogout(User user)
+        {
+            App.UserLogin(user);
+            App.UserLogout();
+            Assert.True(!App.LoggedOut(), "Logout to account failed");
+        }
+    }
+}
